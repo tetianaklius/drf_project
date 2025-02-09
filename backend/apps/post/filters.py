511a4d8a5ee6e76f03b9b9742b1f4293db9e2ac1
tetianaklius <tetyanaklyus@gmail.com>
2025-02-09 22:28:06 +0200

    posts filter update

--- backend/apps/post/filters.py
+++ backend/apps/post/filters.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as filters
 
 
 class PostsFilter(filters.FilterSet):
-    label = filters.CharFilter(field_name="label", lookup_expr="icontains")
+    label = filters.CharFilter(field_name="label", lookup_expr="exact")
     date_from = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
     order = filters.OrderingFilter(
         fields=(
